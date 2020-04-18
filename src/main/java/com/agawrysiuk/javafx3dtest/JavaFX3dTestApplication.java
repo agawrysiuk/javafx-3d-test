3439e23448d16bf5840a1dd9f46639d92d81563e
dlrzz <arkad.gawry@gmail.com>
2020-04-18 22:13:32 +0200

    Creating a sphere

--- src/main/java/com/agawrysiuk/javafx3dtest/JavaFX3dTestApplication.java
+++ src/main/java/com/agawrysiuk/javafx3dtest/JavaFX3dTestApplication.java
@@ -3,7 +3,13 @@ package com.agawrysiuk.javafx3dtest;
 import com.agawrysiuk.javafx3dtest.controller.Controller;
 import com.agawrysiuk.javafx3dtest.view.View;
 import javafx.application.Application;
+import javafx.scene.Camera;
+import javafx.scene.Group;
+import javafx.scene.PerspectiveCamera;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Sphere;
 import javafx.stage.Stage;
 import lombok.extern.slf4j.Slf4j;
 
@@ -20,7 +26,31 @@ public class JavaFX3dTestApplication extends Application {
 		View view = new View(controller);
 		controller.setView(view.getMainView());
 
-		Scene scene = new Scene(view.asParent(), 400, 400);
+		Sphere sphere = new Sphere(250);
+
+		Group group = new Group();
+		group.getChildren().add(sphere);
+
+		Camera camera = new PerspectiveCamera();
+		Scene scene = new Scene(group, 1400, 800);
+		scene.setFill(Color.SILVER);
+		scene.setCamera(camera);
+
+		sphere.translateXProperty().set(1400 / 2);
+		sphere.translateYProperty().set(800 / 2);
+
+		stage.addEventHandler(KeyEvent.KEY_PRESSED, event ->{
+			switch (event.getCode()) {
+				case W:
+					sphere.translateZProperty().set(sphere.getTranslateZ() + 100);
+					break;
+				case S:
+					sphere.translateZProperty().set(sphere.getTranslateZ() - 100);
+					break;
+			}
+		});
+//
+//		Scene scene = new Scene(view.asParent(), 400, 400);
 		stage.setTitle("JavaFX 3d Test");
 //		stage.setMaximized(true);
 		stage.setScene(scene);
