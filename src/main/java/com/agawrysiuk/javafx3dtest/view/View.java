0e17cdbfa22cc76b44490f08d110b7fbc8082ee7
dlrzz <arkad.gawry@gmail.com>
2020-04-19 19:15:00 +0200

    Added PointLight

--- src/main/java/com/agawrysiuk/javafx3dtest/view/View.java
+++ src/main/java/com/agawrysiuk/javafx3dtest/view/View.java
@@ -5,16 +5,18 @@ import com.agawrysiuk.javafx3dtest.utils.TransformGroup;
 import javafx.scene.*;
 import javafx.scene.image.Image;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.PhongMaterial;
 import javafx.scene.shape.Box;
+import javafx.scene.transform.Translate;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 
 @Data
 @Slf4j
 public class View {
-    private BorderPane mainView;
+    private Pane mainView;
     private Scene scene;
     private Box box;
     private TransformGroup group;
@@ -23,7 +25,7 @@ public class View {
 
     public View(Controller controller) {
         this.controller = controller;
-        mainView = new BorderPane();
+        mainView = new Pane();
     }
 
     public Parent asParent() {
@@ -33,9 +35,9 @@ public class View {
     public void createAndConfigurePane() {
         prepareGroup();
         createBox();
-        group.getChildren().add(box);
+        group.getChildren().addAll(box, prepareLightSource());
         prepareCamera();
-        mainView.getChildren().add(group);
+        mainView.getChildren().addAll(group);
     }
 
     private void createBox() {
@@ -43,6 +45,8 @@ public class View {
         PhongMaterial material = new PhongMaterial();
 //        material.setDiffuseColor(Color.ROYALBLUE);
         material.setDiffuseMap(new Image(getClass().getResourceAsStream("/wood.jpg")));
+        material.setSpecularColor(Color.SILVER);
+        material.setSpecularPower(20.0);
         box.setMaterial(material);
     }
 
@@ -58,6 +62,13 @@ public class View {
         Camera camera = new PerspectiveCamera();
         scene.setFill(Color.SILVER);
         scene.setCamera(camera);
+    }
+
+    private LightBase prepareLightSource() {
+        PointLight pointLight = new PointLight();
+        pointLight.setColor(Color.RED);
+        pointLight.getTransforms().add(new Translate(0,-50,-50));
+        return pointLight;
     }
 
 }
